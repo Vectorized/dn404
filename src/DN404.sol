7fb8ad75e232a40ec94b518b410e80108a189ca9
Vectorized <webby1111@hotmail.com>
2024-02-17 06:56:10 +0800

    ⚡️ Mint lookup optimization (#80)
    * Remove id skipping for mints
    
    * Optimize mints with bitmap lookup
    
    * Optimize
    
    * Optimize
    
    * Add fuzz test
    
    * Strengthen test
    
    * Tidy
    
    * Edit comment
    
    * Optimize
    
    * Strengthen test

--- src/DN404.sol
+++ src/DN404.sol
@@ -167,6 +167,8 @@ abstract contract DN404 {
         mapping(uint256 => address) nftApprovals;
         // Bitmap of whether an non-zero NFT approval may exist.
         Bitmap mayHaveNFTApproval;
+        // Bitmap of whether a token ID exists.
+        Bitmap exists;
         // Mapping of user allowances for token spenders.
         AddressPairToUint256RefMap allowance;
         // Mapping of NFT IDs owned by an address.
@@ -380,13 +382,13 @@ abstract contract DN404 {
         AddressData storage toAddressData = _addressData(to);
 
         unchecked {
-            uint256 maxNFTId;
+            uint256 maxId;
             {
                 uint256 totalSupply_ = uint256($.totalSupply) + amount;
                 $.totalSupply = uint96(totalSupply_);
                 uint256 overflows = _toUint(_totalSupplyOverflows(totalSupply_));
                 if (overflows | _toUint(totalSupply_ < amount) != 0) revert TotalSupplyOverflow();
-                maxNFTId = totalSupply_ / _unit();
+                maxId = totalSupply_ / _unit();
             }
             uint256 toEnd;
             {
@@ -415,17 +417,19 @@ abstract contract DN404 {
                         } else {
                             id = nextTokenId;
                             while (_get(oo, _ownershipIndex(id)) != 0) {
-                                id = _wrapNFTId(id + 1, maxNFTId);
+                                id = _useExistsLookup()
+                                    ? _wrapNFTId(_findFirstUnset($.exists, id + 1, maxId + 1), maxId)
+                                    : _wrapNFTId(id + 1, maxId);
                             }
-                            nextTokenId = _wrapNFTId(id + 1, maxNFTId);
+                            nextTokenId = _wrapNFTId(id + 1, maxId);
                         }
+                        if (_useExistsLookup()) _set($.exists, id, true);
                         _set(toOwned, toIndex, uint32(id));
                         _setOwnerAliasAndOwnedIndex(oo, id, toAlias, uint32(toIndex++));
                         _packedLogsAppend(packedLogs, id);
                     } while (toIndex != toEnd);
 
-                    // Leave some spacing between minted batches for better open addressing.
-                    $.nextTokenId = uint32(_wrapNFTId(nextTokenId + 7, maxNFTId));
+                    $.nextTokenId = uint32(nextTokenId);
                     $.burnedPoolSize = uint32(burnedPoolSize);
                     _packedLogsSend(packedLogs, $.mirrorERC721);
                 }
@@ -482,9 +486,8 @@ abstract contract DN404 {
                     uint256 id = _get(fromOwned, --fromIndex);
                     _setOwnerAliasAndOwnedIndex(oo, id, 0, 0);
                     _packedLogsAppend(packedLogs, id);
-                    if (addToBurnedPool) {
-                        _set($.burnedPool, burnedPoolSize++, uint32(id));
-                    }
+                    if (_useExistsLookup()) _set($.exists, id, false);
+                    if (addToBurnedPool) _set($.burnedPool, burnedPoolSize++, uint32(id));
                     if (_get($.mayHaveNFTApproval, id)) {
                         _set($.mayHaveNFTApproval, id, false);
                         delete $.nftApprovals[id];
@@ -562,9 +565,8 @@ abstract contract DN404 {
                     uint256 id = _get(fromOwned, --fromIndex);
                     _setOwnerAliasAndOwnedIndex(oo, id, 0, 0);
                     _packedLogsAppend(packedLogs, id);
-                    if (addToBurnedPool) {
-                        _set($.burnedPool, burnedPoolSize++, uint32(id));
-                    }
+                    if (_useExistsLookup()) _set($.exists, id, false);
+                    if (addToBurnedPool) _set($.burnedPool, burnedPoolSize++, uint32(id));
                     if (_get($.mayHaveNFTApproval, id)) {
                         _set($.mayHaveNFTApproval, id, false);
                         delete $.nftApprovals[id];
@@ -579,7 +581,7 @@ abstract contract DN404 {
                 uint256 toIndex = t.toOwnedLength;
                 uint256 toEnd = toIndex + t.numNFTMints;
                 uint32 toAlias = _registerAndResolveAlias(toAddressData, to);
-                uint256 maxNFTId = t.totalSupply / _unit();
+                uint256 maxId = t.totalSupply / _unit();
                 toAddressData.ownedLength = uint32(toEnd);
                 // Mint loop.
                 do {
@@ -589,17 +591,19 @@ abstract contract DN404 {
                     } else {
                         id = nextTokenId;
                         while (_get(oo, _ownershipIndex(id)) != 0) {
-                            id = _wrapNFTId(id + 1, maxNFTId);
+                            id = _useExistsLookup()
+                                ? _wrapNFTId(_findFirstUnset($.exists, id + 1, maxId + 1), maxId)
+                                : _wrapNFTId(id + 1, maxId);
                         }
-                        nextTokenId = _wrapNFTId(id + 1, maxNFTId);
+                        nextTokenId = _wrapNFTId(id + 1, maxId);
                     }
+                    if (_useExistsLookup()) _set($.exists, id, true);
                     _set(toOwned, toIndex, uint32(id));
                     _setOwnerAliasAndOwnedIndex(oo, id, toAlias, uint32(toIndex++));
                     _packedLogsAppend(packedLogs, id);
                 } while (toIndex != toEnd);
 
-                // Leave some spacing between minted batches for better open addressing.
-                $.nextTokenId = uint32(_wrapNFTId(nextTokenId + 7, maxNFTId));
+                $.nextTokenId = uint32(nextTokenId);
             }
 
             if (packedLogs.logs.length != 0) {
@@ -626,6 +630,15 @@ abstract contract DN404 {
         // Add to burned pool if the load factor > 50%, and collection is not small.
         uint256 thres = (totalSupplyAfterBurn / _unit()) >> 1;
         return _toUint(totalNFTSupplyAfterBurn > thres) & _toUint(thres > 128) != 0;
+    }
+
+    /// @dev Returns whether to use the exists lookup for more efficient
+    /// scanning of an empty token ID slot. Highly recommended for collections
+    /// with near full load factor `totalNFTSupply * _unit() / totalSupply`.
+    /// The trade off is slightly higher initial storage write costs,
+    /// which will be quickly amortized away.
+    function _useExistsLookup() internal pure virtual returns (bool) {
+        return true;
     }
 
     /// @dev Transfers token `id` from `from` to `to`.
@@ -1076,6 +1089,46 @@ abstract contract DN404 {
         }
     }
 
+    /// @dev Returns the index of the least significant unset bit in `[begin, end)`.
+    /// If no set bit is found, returns `type(uint256).max`.
+    function _findFirstUnset(Bitmap storage bitmap, uint256 begin, uint256 end)
+        internal
+        view
+        returns (uint256 unsetBitIndex)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            unsetBitIndex := not(0) // Initialize to `type(uint256).max`.
+            let s := shl(96, bitmap.slot) // Storage offset of the bitmap.
+            let bucket := add(s, shr(8, begin))
+            let lastBucket := add(s, shr(8, end))
+            let negBits := shl(and(0xff, begin), shr(and(0xff, begin), not(sload(bucket))))
+            if iszero(negBits) {
+                for {} 1 {} {
+                    bucket := add(bucket, 1)
+                    negBits := not(sload(bucket))
+                    if or(negBits, gt(bucket, lastBucket)) { break }
+                }
+                if gt(bucket, lastBucket) {
+                    negBits := shr(and(0xff, not(end)), shl(and(0xff, not(end)), negBits))
+                }
+            }
+            if negBits {
+                // Find-first-set routine.
+                let b := and(negBits, add(not(negBits), 1)) // Isolate the least significant bit.
+                let r := shl(7, lt(0xffffffffffffffffffffffffffffffff, b))
+                r := or(r, shl(6, lt(0xffffffffffffffff, shr(r, b))))
+                r := or(r, shl(5, lt(0xffffffff, shr(r, b))))
+                // For the remaining 32 bits, use a De Bruijn lookup.
+                // forgefmt: disable-next-item
+                r := or(r, byte(and(div(0xd76453e0, shr(r, b)), 0x1f),
+                    0x001f0d1e100c1d070f090b19131c1706010e11080a1a141802121b1503160405))
+                r := or(shl(8, sub(bucket, s)), r)
+                unsetBitIndex := or(r, sub(0, or(iszero(lt(r, end)), lt(r, begin))))
+            }
+        }
+    }
+
     /// @dev Returns a storage reference to the value at (`a0`, `a1`) in `map`.
     function _ref(AddressPairToUint256RefMap storage map, address a0, address a1)
         internal
@@ -1094,10 +1147,10 @@ abstract contract DN404 {
     }
 
     /// @dev Wraps the NFT ID.
-    function _wrapNFTId(uint256 id, uint256 maxNFTId) internal pure returns (uint256 result) {
+    function _wrapNFTId(uint256 id, uint256 maxId) internal pure returns (uint256 result) {
         /// @solidity memory-safe-assembly
         assembly {
-            result := or(mul(iszero(gt(id, maxNFTId)), id), gt(id, maxNFTId))
+            result := or(mul(iszero(gt(id, maxId)), id), gt(id, maxId))
         }
     }
 
--- test/Mappings.t.sol
+++ test/Mappings.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 import "./utils/SoladyTest.sol";
 import {LibPRNG} from "solady/utils/LibPRNG.sol";
+import {LibBit} from "solady/utils/LibBit.sol";
 
 contract MappingsTest is SoladyTest {
     using LibPRNG for *;
@@ -115,6 +116,128 @@ contract MappingsTest is SoladyTest {
             let o := and(0xff, index) // Storage slot offset (bits).
             sstore(s, or(and(sload(s), not(shl(o, 1))), shl(o, iszero(iszero(value)))))
         }
+    }
+
+    /// @dev Returns the index of the least significant unset bit in `[begin, end)`.
+    /// If no set bit is found, returns `type(uint256).max`.
+    function _findFirstUnset(Bitmap storage bitmap, uint256 begin, uint256 end)
+        internal
+        view
+        returns (uint256 unsetBitIndex)
+    {
+        /// @solidity memory-safe-assembly
+        assembly {
+            unsetBitIndex := not(0) // Initialize to `type(uint256).max`.
+            let s := shl(96, bitmap.slot) // Storage offset of the bitmap.
+            let bucket := add(s, shr(8, begin))
+            let lastBucket := add(s, shr(8, end))
+            let negBits := shl(and(0xff, begin), shr(and(0xff, begin), not(sload(bucket))))
+            if iszero(negBits) {
+                for {} 1 {} {
+                    bucket := add(bucket, 1)
+                    negBits := not(sload(bucket))
+                    if or(negBits, gt(bucket, lastBucket)) { break }
+                }
+                if gt(bucket, lastBucket) {
+                    negBits := shr(and(0xff, not(end)), shl(and(0xff, not(end)), negBits))
+                }
+            }
+            if negBits {
+                // Find-first-set routine.
+                let b := and(negBits, add(not(negBits), 1)) // Isolate the least significant bit.
+                let r := shl(7, lt(0xffffffffffffffffffffffffffffffff, b))
+                r := or(r, shl(6, lt(0xffffffffffffffff, shr(r, b))))
+                r := or(r, shl(5, lt(0xffffffff, shr(r, b))))
+                // For the remaining 32 bits, use a De Bruijn lookup.
+                // forgefmt: disable-next-item
+                r := or(r, byte(and(div(0xd76453e0, shr(r, b)), 0x1f),
+                    0x001f0d1e100c1d070f090b19131c1706010e11080a1a141802121b1503160405))
+                r := or(shl(8, sub(bucket, s)), r)
+                unsetBitIndex := or(r, sub(0, or(iszero(lt(r, end)), lt(r, begin))))
+            }
+        }
+    }
+
+    function _fillBucket(Bitmap storage bitmap, uint256 i) internal {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let s := add(shl(96, bitmap.slot), i) // Storage slot.
+            sstore(s, not(0))
+        }
+    }
+
+    function testFindFirstUnset() public {
+        assertEq(_findFirstUnset(bitmapA, 0, 1000), 0);
+        _set(bitmapA, 1, true);
+        assertEq(_findFirstUnset(bitmapA, 0, 1000), 0);
+        _set(bitmapA, 0, true);
+        assertEq(_findFirstUnset(bitmapA, 0, 1000), 2);
+        _fillBucket(bitmapA, 0);
+        assertEq(_findFirstUnset(bitmapA, 0, 1000), 256);
+        _set(bitmapA, 256, true);
+        assertEq(_findFirstUnset(bitmapA, 0, 1000), 257);
+
+        assertEq(_findFirstUnset(bitmapB, 500, 1000), 500);
+        _fillBucket(bitmapB, 1); // Set bits `[256, 512)`.
+        assertEq(_findFirstUnset(bitmapB, 500, 1000), 512);
+        assertEq(_findFirstUnset(bitmapB, 500, 513), 512);
+        assertEq(_findFirstUnset(bitmapB, 500, 512), type(uint256).max);
+        assertEq(_findFirstUnset(bitmapB, 256, 512), type(uint256).max);
+        assertEq(_findFirstUnset(bitmapB, 255, 512), 255);
+        assertEq(_findFirstUnset(bitmapB, 255, 255), type(uint256).max);
+        assertEq(_findFirstUnset(bitmapB, 255, 254), type(uint256).max);
+    }
+
+    function testFindFirstUnset(uint256) public {
+        uint256[] memory m = new uint256[](5);
+
+        do {
+            if (_random() % 4 > 0) {
+                uint256 n = _random() % 32;
+                for (uint256 t; t != n; ++t) {
+                    uint256 r = _random() % 1024;
+                    m[r >> 8] |= 1 << (r & 0xff);
+                    _set(bitmapA, r, true);
+                }
+            }
+            if (_random() % 4 > 0) {
+                uint256 n = _random() % 8;
+                for (uint256 t; t != n; ++t) {
+                    uint256 o = _random() % 1024;
+                    uint256 q = _random() % 64;
+                    for (uint256 j; j != q; ++j) {
+                        uint256 r = j + o;
+                        if (r >= 1024) break;
+                        m[r >> 8] |= 1 << (r & 0xff);
+                        _set(bitmapA, r, true);
+                    }
+                }
+            }
+            for (uint256 j; j != 4; ++j) {
+                if (_random() % 8 == 0) {
+                    _fillBucket(bitmapA, j);
+                    m[j] = type(uint256).max;
+                }
+            }
+            do {
+                uint256 begin = _random() % (1024 + 10);
+                uint256 end = _random() % (1024 + 10);
+                uint256 actual = _findFirstUnset(bitmapA, begin, end);
+                uint256 expected = _findFirstUnset(m, begin, end);
+                assertEq(actual, expected);
+            } while (_random() % 16 > 0);
+        } while (_random() % 2 == 0);
+    }
+
+    function _findFirstUnset(uint256[] memory m, uint256 begin, uint256 end)
+        internal
+        pure
+        returns (uint256)
+    {
+        for (uint256 i = begin; i < end; ++i) {
+            if ((m[i >> 8] >> (i & 0xff)) & 1 == 0) return i;
+        }
+        return type(uint256).max;
     }
 
     Bitmap bitmapA;
