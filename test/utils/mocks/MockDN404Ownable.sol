0e2a4630944b97e91f0dcd04f887e547c068cb7c
Vectorized <webby1111@hotmail.com>
2025-01-15 13:28:08 +0000

    T

--- test/utils/mocks/MockDN404Ownable.sol
+++ test/utils/mocks/MockDN404Ownable.sol
@@ -5,12 +5,13 @@ import "./MockDN404.sol";
 import {Ownable} from "solady/auth/Ownable.sol";
 
 contract MockDN404Ownable is MockDN404, Ownable {
-	constructor(address initialOwner) {
-		if (initialOwner != address(0)) {
-			_initializeOwner(initialOwner);
-		}
-	}
+    constructor(address initialOwner) {
+        if (initialOwner != address(0)) {
+            _initializeOwner(initialOwner);
+        }
+    }
+
     function initializeOwner(address initialOwner) public {
-    	_initializeOwner(initialOwner);
+        _initializeOwner(initialOwner);
     }
 }
