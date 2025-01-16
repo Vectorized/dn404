03694831f22bdcf1e2ce5eceb5669aa16af8d518
Vectorized <webby1111@hotmail.com>
2025-01-17 01:34:41 +0800

    ♻️ SkipNFTDefault (#148)

--- src/DN404.sol
+++ src/DN404.sol
@@ -34,6 +34,21 @@ pragma solidity ^0.8.4;
 /// - For MEV safety, users should NOT have concurrently open orders for the ERC20 and ERC721.
 abstract contract DN404 {
     /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
+    /*                           ENUMS                            */
+    /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    /// @dev Denotes the default skip NFT status.
+    /// Override `_skipNFTDefault` to return a preferred value (defaults to `HasCode`).
+    enum SkipNFTDefault {
+        // `On` if the ERC20 owner has code, else `Off`.
+        HasCode,
+        // Skip NFT (ERC20 transfers will NOT trigger NFT minting / burning / transferring).
+        On,
+        // NOT skip NFT (ERC20 transfers will trigger NFT minting / burning / transferring).
+        Off
+    }
+
+    /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
     /*                           EVENTS                           */
     /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
 
@@ -308,6 +323,11 @@ abstract contract DN404 {
     /*«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-«-*/
     /*                       CONFIGURABLES                        */
     /*-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»-»*/
+
+    /// @dev Returns the default mode for the skip NFT status.
+    function _skipNFTDefault() internal view virtual returns (SkipNFTDefault) {
+        return SkipNFTDefault.HasCode;
+    }
 
     /// @dev Returns whether the tokens IDs are from `[1..n]` instead of `[0..n-1]`.
     function _useOneIndexed() internal pure virtual returns (bool) {
@@ -1033,12 +1053,16 @@ abstract contract DN404 {
     /// Returns false otherwise.
     function getSkipNFT(address owner) public view virtual returns (bool result) {
         uint8 flags = _getDN404Storage().addressData[owner].flags;
-        /// @solidity memory-safe-assembly
-        assembly {
-            result := iszero(iszero(and(flags, _ADDRESS_DATA_SKIP_NFT_FLAG)))
-            if iszero(and(flags, _ADDRESS_DATA_SKIP_NFT_INITIALIZED_FLAG)) {
-                result := iszero(iszero(extcodesize(owner)))
+        result = flags & _ADDRESS_DATA_SKIP_NFT_FLAG != 0;
+        if (flags & _ADDRESS_DATA_SKIP_NFT_INITIALIZED_FLAG == uint256(0)) {
+            if (_skipNFTDefault() == SkipNFTDefault.HasCode) {
+                /// @solidity memory-safe-assembly
+                assembly {
+                    result := iszero(iszero(extcodesize(owner)))
+                }
             }
+            if (_skipNFTDefault() == SkipNFTDefault.On) result = true;
+            if (_skipNFTDefault() == SkipNFTDefault.Off) result = false;
         }
     }
 
